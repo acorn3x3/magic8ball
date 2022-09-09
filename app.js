bb91fffcecc7aa68dbaed96c441ac765a0ba2ff9
Andrew Cornejo <atcornejo3@gmail.com>
2022-09-09 15:46:59 -0700

    adding framework and assets

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
+const roll-button = document.getElementById('roll') 
+const reset-button = document.getElementById('reset')
+
+rollButton.addEventListener('click, () => [
+    const randNum = Math.floor(Math.random() * answers.length')
+    rolloutcome.textContent = answers [randNum];
+
+    inputsContainer.classList.toggle (hide)
+    rolloutcomeContainer.classlist.toggle (hide)
+
+    resetbutton.addeventlistener ('click'( => () ))
+
+
+
+
+const answers = [
+  "You've died"
+  "You kill 1 goblin, but die to the rest"
+  "You kill 2 goblins, but die to the rest"
+  "option 4"
+  "option 5"
+  "option 6"
+  "option 7"
+  "option 8"
+  "option 9"
+  "option 10"
+  "option 11"
+  "option 12"
+  "option 13"
+  "option 14"
+  "option 15"
+  "option 16"
+  "option 17"
+  "option 18"
+  "option 19"
+  "critical hit"
+]
+
+
+
+
+
 // import functions and grab DOM elements
 
 // let state
